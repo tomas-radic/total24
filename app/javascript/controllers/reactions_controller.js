1a45e0bd1371c4e58003b52752bd35b9531b97c9
Tomas Radic <tomas.radic@gmail.com>
2025-04-08 22:30:42 +0200

    Fixed bootstrap requirement.

--- app/javascript/controllers/reactions_controller.js
+++ app/javascript/controllers/reactions_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
   }
 
   initTooltip() {
-    // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+    // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
     return new bootstrap.Tooltip(this.reactionsCountTarget)
     // this.reactionsCountTarget.tooltip()
   }
